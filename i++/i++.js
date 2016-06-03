f73ab3fd45856cae18627fb9bea8bcb69b9b1059
Phanuphat Cholatda <remix.tk108@gmail.com>
2016-06-04 01:34:08 +0700

    @poc : i++ and ++i

---
+++ i++/i++.js
@@ -0,0 +1,18 @@
+function hello (str) {
+	console.log('hello ' + str);
+
+	return 'aaa';
+}
+
+console.log('A');
+
+var i;
+var j;
+var k = 0;
+i = 1;
+j = i++;
+++k;
+console.log("i:" + i + ", j:" + j + ", k:" + k);
+
+
+// console.log(console.log('kkkk'));
