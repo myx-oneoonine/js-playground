eaf385bf593658b84398046540655ca36ca9c4ab
Phanuphat Cholatda <remix.tk108@gmail.com>
2016-06-06 00:06:03 +0700

    poc fix ทศนิยม แบบไม่ปัดเศษ

--- js basic/numberFixedPoint.js
+++ js basic/numberFixedPoint.js
@@ -1,3 +1,4 @@
 var num = 123.123456789
 console.log(num.toFixed(4));
 console.log("result of \"(12.55555555).toFixed(4)\" : " + (12.55555555).toFixed(4));
+console.log("result of \"(123.123456789)).toPrecision(6)\" : " + (123.123456789).toPrecision(6));
